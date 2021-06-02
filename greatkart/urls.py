33ef04508b9fd2cb3e9e43dc623b056c5bba049b
Krittin Khaneung <6110210013@psu.ac.th>
2021-06-02 22:45:50 +0700

    crate

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -18,8 +18,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-	path('', views.home, name='home'),
-	path('store/', include('store.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', views.home, name='home'),
+                  path('store/', include('store.urls')),
+                  path('cart/', include('carts.urls'))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
